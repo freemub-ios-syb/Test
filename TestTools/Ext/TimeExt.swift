b4a5bbe699057c844304ab026561049c8887d100
syb <18961846623@163.com>
2017-11-23 10:19:47 +0800

    1.fix TimeExt

---
+++ TestTools/Ext/TimeExt.swift
@@ -0,0 +1,15 @@
+//
+//  TimeExt.swift
+//  TestTools
+//
+//  Created by 舒圆波 on 17/11/23.
+//  Copyright © 2017年 舒圆波. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    func getStringFromDate() -> String {
+        return self.debugDescription
+    }
+}
